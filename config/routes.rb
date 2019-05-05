c40858c3ad51de54b555a4ff616c36fdf1de4864
learn <36858744+decentralvision@users.noreply.github.com>
2019-05-04 23:58:47 -0700

    generated controller and resource for membership model, added associations to user and membership

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 	get '/donate' => 'treasury#donate', as: '/donate'
-  get '/membership' => 'treasury#membership', as: '/membership'
+
 	get '/about' => 'welcome#about', as: '/about'
 	get '/proposals' => 'proposals#index', as: '/proposals'
 
@@ -13,10 +13,14 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'	
 	get '/logout' => 'sessions#destroy'
 
+
+	resources :memberships
+	
 	resources :proposals
 	
 	resources :users do
 		resources :proposals
 	end
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
