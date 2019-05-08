2b4428947e9d370b01e247a77c3d86ce933596d3
Tom K <36858744+decentralvision@users.noreply.github.com>
2019-05-08 09:42:44 -0400

    fixed logic in votes controller create method, added jquery to correct color of vote buttons according to the results of the form submission, moved jquery script from form template for vote button to index page for proposals

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,19 +6,19 @@ class VotesController < ApplicationController
   def new; end
 
   def create
-    vote = Vote.find_or_create_by(user_id: params[:user_id], proposal_id: params[:proposal_id])
-    if vote == current_user.active_vote
-      vote.active = false
-      vote.save
+    @vote = Vote.find_or_create_by(user_id: params[:user_id], proposal_id: params[:proposal_id])
+    if @vote == current_user.active_vote
+      @vote.active = false
     else
-      vote.active = true
-      vote.save
       if current_user.active_vote
-        current_user.active_vote.active = false
-        current_user.active_vote.save
+        current_vote = current_user.active_vote
+        current_vote.active = false
+        current_vote.save
       end
+      @vote.active = true
     end
-    end
+    @vote.save
+  end
 
   def update; end
 
